d16da67829a80205b6825e4063ff8bdbbe62c613
raihanbimaac <raihanbimaac@gmail.com>
2020-10-15 12:17:06 +0700

    Create coba prak2 no4.py

---
+++ coba prak2 no4.py
@@ -0,0 +1,20 @@
+x = 10
+print(type(x))
+y = 20
+print(type(y))
+print(type(x**y))
+x = 10.6
+print(type(x))
+y = 20
+print(type(y))
+print(type(x**y))
+x = 10
+print(type(x))
+y = 20.7
+print(type(y))
+print(type(x**y))
+x = 10.8
+print(type(x))
+y = 20.2
+print(type(y))
+print(type(x**y))
