60a539ce627e337982419de95c4feef0e3ad49f0
raihanbimaac <raihanbimaac@gmail.com>
2020-10-15 12:17:11 +0700

    Create latihan01.py

---
+++ latihan01.py
@@ -0,0 +1,11 @@
+#Program permasalahan Biaya Sewa Rental mobil
+
+jamAmbil=6
+menitAmbil=00
+print('Mobil disewa pada pukul 06.00')
+
+jamBalik=23
+menitBalik=50
+print('Mobil dikembalikan pada pukul 23.50'+'\n')
+
+#Menghitung berapa lama mobil disewa, jika selisih menit lebih dari 30 menit di hitung 1 jam
