e62b24ec8c723e7c34611144ef77e17bfa08b45b
raihanbimaac <raihanbimaac@gmail.com>
2020-10-15 12:17:16 +0700

    Create latihan02.py

---
+++ latihan02.py
@@ -0,0 +1,10 @@
+#Program Untuk Menghitung Jumlah Bahan Bakar Dalam Sebuah Perjalanan
+
+jarakTotal=795
+print('Jarak yang akan ditempuh Pak Budi adalah sejauh 795 km'+ '\n')
+
+jarakPerLiter=12
+print('Untuk 1liter mobil, mobil pak Budi bisa menempuh jarak 12 km'+'\n')
+
+bensinYangDiperlukan=jarakTotal/jarakPerLiter
+print('Bensin Yang Diperlukan adalah'+str(bensinYangDiperlukan)) 
