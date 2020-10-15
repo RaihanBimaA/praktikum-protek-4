1faba3a5f3395c6adfcadd7dd6b56855671293d8
raihanbimaac <raihanbimaac@gmail.com>
2020-10-15 12:17:33 +0700

    Create latihan05.py

---
+++ latihan05.py
@@ -0,0 +1,12 @@
+#program untuk membuat diagram jumlah mahasiswa
+
+print('=== Program Membuat Diagram Jumlah Mahasiswa ==='+ '\n')
+
+lakiLaki=int(input('berapa jumlah mahasiswa laki-laki di jurusan PTIK UNS?'))
+perempuan=int(input('berapa jumlah mahasiswa perempuan di jurusan PTIK UNS?'))
+lk= int(lakiLaki/10)
+pr=int(perempuan/10)
+print('\n')
+
+print('Laki-laki='+'*'*lk)
+print('perempuan='+'*'*pr)
